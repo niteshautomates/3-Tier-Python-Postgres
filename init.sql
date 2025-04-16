21594189e4cf2a9bb9a31d2a023731f89286f9ad
Ubuntu <ubuntu@ip-172-31-47-206.ap-south-1.compute.internal>
2025-04-16 19:18:49 +0000

    modified pg server host

--- init.sql
+++ init.sql
@@ -1,5 +1,15 @@
- CREATE USER root WITH PASSWORD 'root';
-CREATE DATABASE my_database;
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_roles WHERE rolname = 'root') THEN
+        CREATE USER root WITH PASSWORD 'root';
+    END IF;
+END$$;
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'my_database') THEN
+        CREATE DATABASE my_database;
+    END IF;
+END$$;
 GRANT ALL PRIVILEGES ON DATABASE my_database TO root;
 \c my_database
 GRANT ALL PRIVILEGES ON SCHEMA public TO root;
